a670aa6395f316f37939c4ede55d2f01cff77ae1
BohdanCh-w <bohdanch.w@gmail.com>
2021-10-26 12:50:31 +0300

    Add second

---
+++ CS-lab3-2async/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lab3_2async {
+    class Program {
+        static async Task Main(string[] args) {
+            return;
+        }
+    }
+}
